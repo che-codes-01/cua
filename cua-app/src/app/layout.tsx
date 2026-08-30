1c7b6134011033826bd9dd2354edd426ba872cb3
che-codes-01 <chetanya.saini250910@gmail.com>
2026-08-30 17:58:20 +0530

    adds components

--- cua-app/src/app/layout.tsx
+++ cua-app/src/app/layout.tsx
@@ -1,19 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
+import Header from "@/components/custom/Header";
+import Footer from "@/components/custom/Footer";
+import { ThemeProvider } from "@/components/theme-provider";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'});
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "cua-app",
@@ -27,10 +20,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={cn("font-sans", inter.variable)}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className="">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark" // 👈 Change this to "light", "dark", or "system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+
+          {children}
+
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
---
+++ cua-app/src/components/custom/LoginCard.tsx
@@ -0,0 +1,32 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import GitHubSignInButton from "./GitHubSignInButton";
+
+function LoginCard({
+  loginWithGithubText = "Login with GitHub",
+  loginCardTitle = "Login to your account",
+  loginCardDescription = "Sign up or Login via your GitHub account!",
+}: {
+  loginWithGithubText?: string;
+  loginCardTitle?: string;
+  loginCardDescription?: string;
+}) {
+  return (
+    <Card className="w-full max-w-sm border">
+      <CardHeader>
+        <CardTitle>{loginCardTitle}</CardTitle>
+        <CardDescription>{loginCardDescription}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <GitHubSignInButton buttonText={loginWithGithubText} />
+      </CardContent>
+    </Card>
+  );
+}
+
+export default LoginCard;
